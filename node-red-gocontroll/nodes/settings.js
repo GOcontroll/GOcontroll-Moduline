650552415f22e987407245def54091d60270180d
RickG <34604504+RickGijsberts@users.noreply.github.com>
2019-12-16 09:44:43 +0100

    Changed percistency for CAN settings

--- node-red-gocontroll/nodes/settings.js
+++ node-red-gocontroll/nodes/settings.js
@@ -4,20 +4,20 @@ module.exports = function(RED) {
 	function GOcontrollSettings(config) {
 	   RED.nodes.createNode(this,config);
 	 
-	 const fs = require('fs');
+	const fs = require('fs');
 	const shell = require('shelljs');
 	
 	var node = this;
 	this.ssid = config.ssid;
 	this.pass = config.pass;
-	this.speedc1 = config.speedc1;
-	this.speedc2 = config.speedc2;
+	const speedc1 = config.speedc1;
+	const speedc2 = config.speedc2;
 	
 		
 	this.log(node.name);
 	
-	node.warn("Setup can0");
-	
+	//node.warn("Setup can0");
+	/*
 	shell.exec('ip link set down can0');
 	shell.exec('ip link set down can1');
 	
@@ -26,7 +26,7 @@ module.exports = function(RED) {
 	
 	shell.exec('ip link set up can0');
 	shell.exec('ip link set up can1');	
-	
+	*/
 	
 	
 	
@@ -41,13 +41,40 @@ module.exports = function(RED) {
 			node.log('The file has been saved!');
 		});
 		
-		var formatted = data.replace(/^wpa_passphrase=.*$/m, 'wpa_passphrase='+ node.pass);
+		formatted = data.replace(/^wpa_passphrase=.*$/m, 'wpa_passphrase='+ node.pass);
 
 		fs.writeFile('/etc/hostapd/hostapd.conf', formatted, 'utf8', function (err) {
 			if (err) throw err;
 
 			node.log('The file has been saved!');
 		});
+		
+	});
+	
+		fs.readFile('/etc/network/interfaces', 'utf8', function (err,data) {
+			
+		let can0 = new RegExp('^.*#can0.*$', 'gm');
+		let can1 = new RegExp('^.*#can1.*$', 'gm');
+		
+		var formatted = data.replace(can0, 'pre-up /sbin/ip link set $IFACE type can bitrate '+ speedc1 + ' triple-sampling on loopback off #can0');
+		
+
+		fs.writeFile('/etc/network/interfaces', formatted, 'utf8', function (err) {
+			if (err) throw err;
+
+			node.log('CAN 1 settings saved!');
+		});
+		
+		
+		formatted = data.replace(can1, 'pre-up /sbin/ip link set $IFACE type can bitrate '+ speedc2 + ' triple-sampling on loopback off #can1');
+		
+				fs.writeFile('/etc/network/interfaces', formatted, 'utf8', function (err) {
+			if (err) throw err;
+
+			node.log('CAN 2 settings saved!');
+		});
+		
+		
 		
 	});
 	
