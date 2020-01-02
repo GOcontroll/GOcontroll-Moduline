a481c7f1b470d524de4d0549c54a63da30be2566
RickG <34604504+RickGijsberts@users.noreply.github.com>
2020-01-02 17:21:55 +0100

    Added node for updating GOcontroll nodes

---
+++ node-red-gocontroll/nodes/software_version.js
@@ -0,0 +1,95 @@
+module.exports = function(RED) {
+    "use strict"
+
+	function GOcontrollSoftware(config) {
+	   RED.nodes.createNode(this,config);
+	 
+	const fs = require('fs');
+	const shell = require('shelljs');
+	
+	var node = this;
+
+	var jsonContent;
+
+	var machineState = 0;
+
+	var msgOut = {};
+	
+	var latestVersion
+	
+	msgOut["machineState"] = machineState;
+
+	Settings_GetVersion ();
+
+
+	
+	
+	
+	
+	/***************************************************************************************
+	** \brief
+	**
+	**
+	** \param
+	** \param
+	** \return
+	**
+	****************************************************************************************/
+	function Settings_GetVersion (){
+		var packageFile = fs.readFileSync("/usr/node-red-gocontroll/package.json")
+		
+		jsonContent = JSON.parse(packageFile);
+		
+		node.status({fill:"green",shape:"dot",text:"GOcontroll node version: "+jsonContent.version});
+	}
+	
+	
+	
+	this.on('input', function(msg) {
+			/* Request the latest version from Github */
+			if(machineState == 0)
+			{
+			const command = '/usr/node-red-gocontroll/nodes/software_version.sh'
+				
+			latestVersion = shell.exec(command);
+			
+			var latestVersion3 = latestVersion.substring(1,latestVersion.length-1);
+			node.warn(latestVersion3);
+			node.warn(jsonContent.version);
+			
+				if(jsonContent.version == latestVersion3)
+				{
+				node.status({fill:"green",shape:"dot",text:"Latest version("+jsonContent.version+") already installed"});
+				machineState = 2
+				}
+				else
+				{
+				machineState = 1	
+				}
+			}
+			else if(machineState == 1)
+			{
+			const command = 'wget -O - https://raw.githubusercontent.com/Rick-GO/GOcontroll-Moduline-III/tree/'+latestVersion.substring(0,latestVersion.length-1)+'/installer/gocontroll-node-update | bash'	
+				
+			node.warn(command);
+			/* routine to update controller */
+			//shell.exec(command);
+			}
+		
+		msgOut["machineState"] = machineState;
+        });
+		
+		
+
+			RED.httpAdmin.get("/softwareVersion", RED.auth.needsPermission('softwareVersion.read'), function(req,res) {
+			res.json(msgOut);
+		});
+		
+		
+		
+    }
+	RED.nodes.registerType("Software",GOcontrollSoftware);
+	
+	
+
+}
---
+++ node-red-gocontroll/nodes/software_version.sh
@@ -0,0 +1,1 @@
+curl --silent "https://api.github.com/repos/Rick-GO/GOcontroll-Moduline-III/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'
