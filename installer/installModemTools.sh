f823cd1147f211e8db6d8939ef37288a63c7aedd
RickG <34604504+RickGijsberts@users.noreply.github.com>
2020-01-31 12:19:52 +0100

    Deleted the manual installers

--- installer/installModemTools.sh
+++
@@ -1,9 +0,0 @@
-YELLOW='\033[33m'
-NORMAL='\033[0m'
-
-echo -e "${YELLOW}Install libqmi tools ${NORMAL}"
-apt-get -y install libqmi-utils
-
-echo -e "${YELLOW}Install udhcpc ${NORMAL}"
-apt-get -y install udhcpc
-
