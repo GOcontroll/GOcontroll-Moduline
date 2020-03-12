c234ced7c752d8c2662358f380b18338deea06ac
RickG <34604504+RickGijsberts@users.noreply.github.com>
2020-03-12 08:24:37 +0100

    Added http static

--- settings.js
+++ settings.js
@@ -106,7 +106,7 @@ module.exports = {
     // When httpAdminRoot is used to move the UI to a different root path, the
     // following property can be used to identify a directory of static content
     // that should be served at http://localhost:1880/.
-    //httpStatic: '/home/nol/node-red-static/',
+    httpStatic: '/usr/node-red-static/',
 
     // The maximum size of HTTP request that will be accepted by the runtime api.
     // Default: 5mb
