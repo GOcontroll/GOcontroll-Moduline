638a986c5ab156e2663e21788d2a65abeae2d1f7
RickG <rickgijsberts@gocontroll.com>
2021-11-12 12:44:36 +0100

    Values are now alway written to memory

--- usr/node-red-gocontroll/nodes/write_memory.js
+++ usr/node-red-gocontroll/nodes/write_memory.js
@@ -54,14 +54,14 @@ module.exports = function(RED) {
 			for(var prop in msg) {
 				if (msg.hasOwnProperty(prop)) {
 					
-					if(msg[prop] != oldValue[prop])
-					{
-					oldValue[prop] = msg[prop];
+					//if(msg[prop] != oldValue[prop])
+					//{
+					//oldValue[prop] = msg[prop];
 					fs.writeFile(path+prop, String(msg[prop].toFixed(decimal)), (err) => {
 						if (err) throw err;
 						//console.log('The file has been saved!');
 						});
-					}
+					//}
 				}
 			}
 		}
@@ -69,25 +69,25 @@ module.exports = function(RED) {
 		/* If key is provided, use the specific key to send data */
 		else if(msg[key] != NaN && inputType === "object")
 		{
-			if(msg[key] != oldValue[key])
-			{
-				oldValue[key] = msg[key];
+			//if(msg[key] != oldValue[key])
+			//{
+				//oldValue[key] = msg[key];
 				fs.writeFile(path+key, String(msg[key].toFixed(decimal)), (err) => {
 				if (err) throw err;
 				//console.log('The file has been saved!');
 				});
-			}
+			//}
 		}
 		else if (msg.payload != NaN)
 		{
-			if(msg.payload != oldValue[key])
-			{
-				oldValue[key] = msg.payload;
+			//if(msg.payload != oldValue[key])
+			//{
+			//	oldValue[key] = msg.payload;
 				fs.writeFile(path+key, String(msg.payload.toFixed(decimal)), (err) => {
 				if (err) throw err;
 				//console.log('The file has been saved!');
 				});
-			}
+			//}
 		}
 			
     });
