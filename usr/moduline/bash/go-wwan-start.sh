573578e43f99cdc227ffb08b871953ed3c35b5e6
Rick-GO <rickgijsberts@gocontroll.com>
2022-07-24 13:35:03 +0200

    Updated script that powercycles the modem when restarting

--- usr/moduline/bash/go-wwan-start.sh
+++ usr/moduline/bash/go-wwan-start.sh
@@ -6,6 +6,20 @@
 # - SIM7000 PCIe -> LTE after commands AT+CMNB=1 and AT+CNMP=38
 #
 
+# Check if hardware is Moduline Mini controller. In this case, LDO and Level shifter needs to be enabled
+if [[ $(tr -d '\0' < /sys/firmware/devicetree/base/hardware) == "Moduline Mini V1.03" ]] ||
+[[ $(tr -d '\0' < /sys/firmware/devicetree/base/hardware) == "Moduline Mini V1.04" ]]||
+[[ $(tr -d '\0' < /sys/firmware/devicetree/base/hardware) == "Moduline Mini V1.05" ]]||
+[[ $(tr -d '\0' < /sys/firmware/devicetree/base/hardware) == "Moduline Mini V1.06" ]]; then
+# Start with powering the module and level shifter
+echo 0 > /sys/devices/platform/leds/leds/communication-ldo-active/brightness
+echo 0 > /sys/devices/platform/leds/leds/sim7000-level-active/brightness
+else
+# Start with powering the module
+echo 0 > /sys/devices/platform/leds/leds/ldo-sim7000/brightness
+fi
+
+sleep 4
 
 # Check if hardware is Moduline Mini controller. In this case, LDO and Level shifter needs to be enabled
 if [[ $(tr -d '\0' < /sys/firmware/devicetree/base/hardware) == "Moduline Mini V1.03" ]] ||
@@ -34,25 +48,6 @@ if ! grep -R "^[#]*\s*${thekey}=.*" $filename > /dev/null; then
 else
   sed -ir "s/^[#]*\s*${thekey}=.*/$thekey=$newvalue/" $filename
 fi
-
-# Wait two seconds to get active
-sleep 5
-# Set serial port to 115200 baudrate 
-stty 115200 < /dev/ttymxc1
-# Send AT command for synchronisation
-echo -ne 'AT\r' > /dev/ttymxc1
-# Wait one second
-sleep 1
-# Send second AT command for synchronisation
-echo -ne 'AT\r' > /dev/ttymxc1
-# Wait one second
-sleep 1 
-# Use Cat-M Technology instead of NB-IoT
-echo -ne 'AT+CMNB=1\r' > /dev/ttymxc1
-# Wait one second
-sleep 1 
-# Use LTE communication technology
-echo -ne 'AT+CNMP=38\r' > /dev/ttymxc1
 
 # Create connection descriptor
 nmcli connection add type gsm ifname 'cdc-wdm0' con-name 'GO-celular' apn 'super' connection.autoconnect yes gsm.pin 0000
--- usr/moduline/bash/go-wwan-stop.sh
+++ usr/moduline/bash/go-wwan-stop.sh
@@ -16,8 +16,15 @@ fi
 
 retrievedKey=$(sed -rn 's/^GO_GPS=([^\n]+)$/\1/p' /usr/moduline/moduline.properties) 
 
-if [[ $retrievedKey == "NOT-ACTIVE" ]]
-then
-	# Stop with powering the module only if GPS is not using it.
-	echo 0 > /sys/devices/platform/leds/leds/ldo-sim7000/brightness
+# Check if hardware is Moduline Mini controller. In this case, LDO and Level shifter needs to be enabled
+if [[ $(tr -d '\0' < /sys/firmware/devicetree/base/hardware) == "Moduline Mini V1.03" ]] ||
+[[ $(tr -d '\0' < /sys/firmware/devicetree/base/hardware) == "Moduline Mini V1.04" ]]||
+[[ $(tr -d '\0' < /sys/firmware/devicetree/base/hardware) == "Moduline Mini V1.05" ]]||
+[[ $(tr -d '\0' < /sys/firmware/devicetree/base/hardware) == "Moduline Mini V1.06" ]]; then
+# Start with powering the module and level shifter
+echo 0 > /sys/devices/platform/leds/leds/communication-ldo-active/brightness
+echo 0 > /sys/devices/platform/leds/leds/sim7000-level-active/brightness
+else
+# Start with powering the module
+echo 0 > /sys/devices/platform/leds/leds/ldo-sim7000/brightness
 fi
