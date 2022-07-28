04f4880c85f49eb19a889dc57c5fd178083c5bf6
SpieringsAE <ben_spierings@hotmail.com>
2022-07-28 09:23:04 +0200

    install fixes
    small tweak to first install script

--- usr/moduline/python/initLed.py
+++ usr/moduline/python/initLed.py
@@ -3,7 +3,7 @@ address = 20
 with SMBus(2) as bus:
 	bus.write_i2c_block_data(address,23,[255])
 	bus.write_i2c_block_data(address,0,[64])
-	# bus.write_i2c_block_data(address, 0x0D, [0])
-	# bus.write_i2c_block_data(address, 0x0B, [165])
-	# bus.write_i2c_block_data(address, 0x0C, [50])
-	bus.write_i2c_block_data(address, 0x0D, [127])
+	bus.write_i2c_block_data(address, 0x0D, [0])
+	bus.write_i2c_block_data(address, 0x0B, [165])
+	bus.write_i2c_block_data(address, 0x0C, [50])
+	# bus.write_i2c_block_data(address, 0x0D, [127])
