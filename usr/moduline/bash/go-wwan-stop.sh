5985e5736022f7c5aa8eab42996aa5873d72803f
Rick-GO <rickgijsberts@gocontroll.com>
2022-10-28 08:16:08 +0200

    Cleaned up script that set property in properties file

--- usr/moduline/bash/go-wwan-stop.sh
+++ usr/moduline/bash/go-wwan-stop.sh
@@ -3,27 +3,13 @@
 # Delete connection descriptor
 # nmcli connection delete GO-celular
 
-# WANN functionality is not claiming the supply anymore so set property
-filename=/usr/moduline/moduline.properties
-thekey="GO_WWAN"
-newvalue="NOT-ACTIVE"
-
-if ! grep -R "^[#]*\s*${thekey}=.*" $filename > /dev/null; then
-  echo "$thekey=$newvalue" >> $filename
+# Switch off LDO regulator
+if [[ $(tr -d '\0' < /sys/firmware/devicetree/base/hardware) == "Moduline Mini"* ]]; then
+echo 0 > /sys/devices/platform/leds/leds/sim7000-level-active/brightness
 else
-  sed -ir "s/^[#]*\s*${thekey}=.*/$thekey=$newvalue/" $filename
+echo 0 > /sys/devices/platform/leds/leds/ldo-sim7000/brightness
 fi
+# Get PWR and RST pin in low state
+echo 0 > /sys/devices/platform/leds/leds/pwr-sim7000/brightness
+echo 0 > /sys/devices/platform/leds/leds/rst-sim7000/brightness
 
-retrievedKey=$(sed -rn 's/^GO_GPS=([^\n]+)$/\1/p' /usr/moduline/moduline.properties) 
-
-if [[ $retrievedKey == "NOT-ACTIVE" ]]
-then
-  if [[ $(tr -d '\0' < /sys/firmware/devicetree/base/hardware) == "Moduline Mini"* ]]; then
-  echo 0 > /sys/devices/platform/leds/leds/sim7000-level-active/brightness
-  else
-  # Start with powering the module
-  echo 0 > /sys/devices/platform/leds/leds/ldo-sim7000/brightness
-  fi
-  echo 0 > /sys/devices/platform/leds/leds/pwr-sim7000/brightness
-  echo 0 > /sys/devices/platform/leds/leds/rst-sim7000/brightness
-fi
