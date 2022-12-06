4375fad242331792a7f4766f1db8f65f747c6a75
SpieringsAE <ben_spierings@hotmail.com>
2022-12-06 08:44:52 +0100

    fixed nodered crash if model is not running
    if the simulink model was not running nodered would crash when trying to process_read().
    node now goes into a loop to check if a model is running if it can't process_read().

--- usr/node-red-gocontroll/nodes/read_simulink_signal.js
+++ usr/node-red-gocontroll/nodes/read_simulink_signal.js
@@ -8,8 +8,9 @@ const shell = require("shelljs");
 
 function GOcontrollReadSimulink(config) {
     RED.nodes.createNode(this,config);
+    let simulink = false;
     let res
-    var interval = null;
+    var intervalRead,intervalCheck = null;
     var node = this;
     const sampleTime = config.sampleTime;
     const outputKey = config.KeyOut;
@@ -20,24 +21,47 @@ function GOcontrollReadSimulink(config) {
     let pid = "";
     let pid1 = "";
     let pid2 = "";
-    pid1 = shell.exec("ps -ef|grep gocontroll.elf | awk {'print $2'}");
-    pid1 = pid1.stdout.split("\n")[0];
-    pid2 = shell.exec("ps -ef|grep gocontroll.elf | awk {'print $2'}");
-    pid2 = pid2.stdout.split("\n")[0];
-    if (pid1==pid2){
-        pid = pid1;
+
+    intervalCheck = setInterval(check_model,2000);
+    
+    
+
+    //Check if the simulink model is running and get its PID
+    function check_model() {
+        console.log("checking if model is running.")
+        pid1 = shell.exec("ps -ef|grep gocontroll.elf | awk {'print $2'}");
+        pid1 = pid1.stdout.split("\n")[0];
+        pid2 = shell.exec("ps -ef|grep gocontroll.elf | awk {'print $2'}");
+        pid2 = pid2.stdout.split("\n")[0];
+        if (pid1==pid2){
+            pid = pid1;
+            simulink = true;
+            intervalRead = setInterval(readSignal, parseInt(sampleTime));
+            clearInterval(intervalCheck);
+            console.log("model is running")
+        }
     }
 
-    interval = setInterval(readSignal, parseInt(sampleTime));
-
+    //Read the signal from the found pid
     function readSignal() {
-        res = uiojs.process_read(parseInt(pid), asap_signal);
-        msgOut={[outputKey]:res};
-        node.send(msgOut);
+        console.log("reading signal"+simulink)
+        if (simulink == true){
+            try{
+                res = uiojs.process_read(parseInt(pid), asap_signal);
+                msgOut={[outputKey]:res};
+                node.send(msgOut);  
+            } catch(err) {
+                simulink = false;
+            }
+        } else {
+            intervalCheck = setInterval(check_model,2000);
+            clearInterval(intervalRead);
+        }
     }
 
     node.on('close', function(done) {
-        clearInterval(interval);
+        clearInterval(intervalRead);
+        clearInterval(intervalCheck);
         done();
     })
 
