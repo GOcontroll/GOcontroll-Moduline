cb5032e2de7628de74e3cc0fa69b5d82f3e76bdd
SpieringsAE <ben_spierings@hotmail.com>
2023-03-24 14:55:58 +0100

    tiny fix in boolean logic

--- etc/controller_update/controller_update_rollback.py
+++ etc/controller_update/controller_update_rollback.py
@@ -3,7 +3,7 @@ import os
 
 arg = input("are you sure you want to undo the last update? y/n: ")
 
-if arg is "y" or arg is "Y":
+if arg == "y" or arg == "Y":
     with open("/etc/controller_update/backed-up-files.txt", "r") as backup:
         for line in backup.readlines():
             try:
