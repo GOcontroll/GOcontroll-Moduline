3069add9be5504ab0ff4810a2e71b027693a6d0e
SpieringsAE <ben_spierings@hotmail.com>
2023-06-28 14:10:20 +0200

    updated kl15 monitor to dynamic lib
    updated parse a2l to also parse the now fixed headers with the new blockset

--- usr/moduline/python/parse_a2l.py
+++ usr/moduline/python/parse_a2l.py
@@ -7,8 +7,10 @@ from os import path
 reached_end = False
 reading_parameter = False
 reading_signal = False
+reading_header = False
 parameters = {}
 signals = {}
+header = {}
 size = 1
 
 a2lLoc = glob("/usr/simulink/*.a2l")
@@ -23,25 +25,51 @@ except:
 with open(a2lLoc, "r") as a2l:
     while not reached_end:
         line = a2l.readline()
-        if reading_parameter or reading_signal:
+
+        if reading_header:
+
+            #get address
+            if "ADDR_EPK" in line:
+                address = int(line.split("x")[1],16)
+                if address == 0:
+                    reading_header = False #if the a2l is not made with the updated post processing function stop trying to read the header
+                continue
+
+            #get the XCP identifier
+            elif "EPK" in line:
+                value = line.split('\"')[1]
+                continue
+
+            #get the string length and save information
+            elif "XcpStationID string length" in line:
+                header = {
+                    'address':address,
+                    'type': asap_datatypes.uint8,
+                    'size': int(line.split('\"')[-2]),
+                    'value': value
+                }
+                reading_header = False
+
+        elif reading_parameter or reading_signal:
+
+            #get name
             if "Name" in line:
-                # print(line)
-                # print(line.split(" "))
                 if reading_parameter:
                     name = line.split(" ")[-3]
                 else:
                     name = line.split(" ")[-1][:-1]
-                # print(name)
+                continue
+
+            #get address
             elif "ECU" in line:
-                # print(line)
-                # print(line.split(" "))
                 if reading_parameter:
                     address = int(line.split(" ")[-2],16)
                 else:
                     address = int(line.split(" ")[-1][:-1],16)
-                # print(address)
+                continue
+
+            #get datatype
             elif "Conversion" in line:
-                # print(line)
                 if "CM_double" in line:
                     dataType = asap_datatypes.double
                 elif "CM_single" in line:
@@ -62,12 +90,14 @@ with open(a2lLoc, "r") as a2l:
                     dataType = asap_datatypes.uint64
                 elif "CM_int64" in line:
                     dataType = asap_datatypes.int64
-                # print(dataType)
+                continue
+
+            #get arraysize
             elif "NUMBER" in line:
-                # print(line)
-                # print(line.split(" "))
                 size = int(line.split(" ")[-1][:-1])
-                # print(size)
+                continue
+
+            #save information
             elif "/end" in line:
                 if reading_parameter:
                     parameters[name] = {
@@ -76,6 +106,7 @@ with open(a2lLoc, "r") as a2l:
                         'size': size
                     }
                     reading_parameter=False
+
                 elif reading_signal:
                     signals[name] = {
                         'address':address,
@@ -83,7 +114,11 @@ with open(a2lLoc, "r") as a2l:
                         'size': size
                     }
                     reading_signal=False
+
                 size =1
+                continue
+
+        #check if an important datafield is starting
         elif "/begin" in line:
             if "CHARACTERISTIC" in line:
                 reading_parameter=True
@@ -91,6 +126,8 @@ with open(a2lLoc, "r") as a2l:
                 reading_signal=True
             elif "COMPU_METHOD" in line:
                 reached_end=True
+            elif "MOD_PAR" in line:
+                reading_header=True
 
 json_parameters = dump_json(dict(sorted(parameters.items())), indent=4)
 with open("/usr/simulink/parameters.json", "w") as parameterFile:
@@ -100,4 +137,9 @@ json_signals = dump_json(dict(sorted(signals.items())), indent=4)
 with open("/usr/simulink/signals.json", "w") as signalFile:
     signalFile.write(json_signals)
 run(["cp", "/usr/simulink/signals.json", "/usr/node-red-static/signals.json"])
+
+json_header = dump_json(dict(sorted(header.items())), indent=4)
+with open("/usr/simulink/header.json", "w") as headerFile:
+    headerFile.write(json_header)
+
 print("a2l file parsed succesfully!")
